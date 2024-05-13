c8672afc3a3c6220d81113daf9d0da0720e5dc67
danielhe4rt <hey@danielheart.dev>
2024-05-13 12:51:32 -0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,48 @@
-use std::io::{Read, Write};
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
 
-fn main() {
+
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
+
+#[tokio::main]
+async fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
     //
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-    for stream in listener.incoming() {
+    loop {
+        let stream = listener.accept().await;
         match stream {
-            Ok(mut stream) => {
-                let mut buffer = [0; 1024];
-
+            Ok((stream, _)) => {
                 println!("accepted new connection");
-                println!("stream: {:?}", stream);
-                loop {
-                    let read_count = stream.read(&mut buffer).unwrap();
-                    if read_count == 0 {
-                        break;
-                    }
-                    println!("read_count: {:?}", read_count);
-
-                    stream.write(b"+PONG\r\n").unwrap();
-                }
-
-
-                //println!("received data: {:?}", buffer);
-
+                tokio::spawn(handle_client(stream));
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+}
+
+
+async fn handle_client(mut stream: TcpStream) {
+    {
+        let mut buffer = [0; 1024];
+
+        println!("stream: {:?}", stream);
+        loop {
+            let read_count = stream.read(&mut buffer).await.unwrap();
+            if read_count == 0 {
+                break;
+            }
+            println!("read_count: {:?}", read_count);
+
+            stream.write(b"+PONG\r\n").await.unwrap();
+        }
+
+        println!("received data: {:?}", buffer);
     }
 }
